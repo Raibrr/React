c96cbca9a19b136f817a8611c662b6f38d2404bd
Ramon Ibarra <ramon.ibrr@gmail.com>
2024-09-11 11:08:23 -0600

    Finish 9 secction

--- hooks-practice/src/main.jsx
+++ hooks-practice/src/main.jsx
@@ -10,11 +10,12 @@ import './index.css'
 // import { FocusScreen } from './04-useRef/FocusScreen'
 // import { Layout } from './05-useLayoutEffect/Layout'
 // import { Memorize } from './06-memos/Memorize'
-import { MemoHook } from './06-memos/MemoHook'
-import { CallbackHook } from './06-memos/CallbackHook'
+// import { MemoHook } from './06-memos/MemoHook'
+// import { CallbackHook } from './06-memos/CallbackHook'
+import { Padre } from './07-tarea-memo/Padre'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <CallbackHook />
-  </React.StrictMode>,
+  //<React.StrictMode>
+    <Padre />
+  //</React.StrictMode>,
 )
